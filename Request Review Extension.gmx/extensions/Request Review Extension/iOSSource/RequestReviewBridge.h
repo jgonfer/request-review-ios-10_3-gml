2c3ccee088de0f0a33bcdc1b7422585022650e42
j_gonfer <josep.gonfer@gmail.com>
2017-03-29 22:16:29 +0200

    Added script and iOS method to request a review

---
+++ Request Review Extension.gmx/extensions/Request Review Extension/iOSSource/RequestReviewBridge.h
@@ -0,0 +1,16 @@
+//
+//  RequestReviewBridge.h
+//  Request Review Extension
+//
+//  Created by Josep Gonzalez Fernandez on 29/03/17.
+//  Copyright © 2017 jgonfer.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RequestReviewBridge : NSObject
+{
+}
+
+- (double) requestReviewAppStore;
+@end
